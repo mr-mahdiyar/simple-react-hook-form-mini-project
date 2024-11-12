842a9da2934a5c6ca4a48a91363a063e4fea88dd
Mahdiyar <m.poursalari3200@gmail.com>
2024-11-12 10:10:50 +0330

    register inputs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,12 @@
 import { useForm } from "react-hook-form";
 export default function Home() {
   const form = useForm();
+  const { register } = form;
   return (
     <div className="flex justify-center items-center min-h-screen w-full">
       <form className="flex flex-col max-w-96 gap-y-8 border rounded-md p-16">
-        <input type="text" placeholder="user name" className="border rounded-md p-2 outline-none" />
-        <input type="text" placeholder="password" className="border rounded-md p-2 outline-none" />
+        <input type="text" placeholder="user name" className="border rounded-md p-2 outline-none" {...register("username")}/>
+        <input type="text" placeholder="password" className="border rounded-md p-2 outline-none" {...register("password")}/>
         <button type="submit" className="p-2 border rounded-md hover:bg-slate-100">
           submit
         </button>
