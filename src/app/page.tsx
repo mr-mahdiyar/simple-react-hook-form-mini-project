df98872fb88da3f5925f0426f7faf495c0dd87a1
Mahdiyar <m.poursalari3200@gmail.com>
2024-11-12 10:14:10 +0330

    add form submition handler

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,20 @@
 "use client";
+interface FormValues {
+  username: string;
+  password: string;
+}
 import { useForm } from "react-hook-form";
 export default function Home() {
-  const form = useForm();
-  const { register } = form;
+  const form = useForm<FormValues>();
+  const { register, handleSubmit } = form;
+
+
+  function submitForm(data: FormValues) {
+    console.log(data)
+  }
   return (
     <div className="flex justify-center items-center min-h-screen w-full">
-      <form className="flex flex-col max-w-96 gap-y-8 border rounded-md p-16">
+      <form className="flex flex-col max-w-96 gap-y-8 border rounded-md p-16" onSubmit={handleSubmit(submitForm)}>
         <input type="text" placeholder="user name" className="border rounded-md p-2 outline-none" {...register("username")}/>
         <input type="text" placeholder="password" className="border rounded-md p-2 outline-none" {...register("password")}/>
         <button type="submit" className="p-2 border rounded-md hover:bg-slate-100">
