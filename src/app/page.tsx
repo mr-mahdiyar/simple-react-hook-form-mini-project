46477d6e77d1807ba6f4f463a59b7463a4478ef2
Mahdiyar <m.poursalari3200@gmail.com>
2024-11-12 10:57:33 +0330

    simple form validation was added.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,15 +8,29 @@ export default function Home() {
   const form = useForm<FormValues>();
   const { register, handleSubmit } = form;
 
-
   function submitForm(data: FormValues) {
-    console.log(data)
+    console.log(data);
   }
   return (
     <div className="flex justify-center items-center min-h-screen w-full">
-      <form className="flex flex-col max-w-96 gap-y-8 border rounded-md p-16" onSubmit={handleSubmit(submitForm)}>
-        <input type="text" placeholder="user name" className="border rounded-md p-2 outline-none" {...register("username")}/>
-        <input type="text" placeholder="password" className="border rounded-md p-2 outline-none" {...register("password")}/>
+      <form className="flex flex-col max-w-96 gap-y-8 border rounded-md p-16" onSubmit={handleSubmit(submitForm)} noValidate>
+        <input
+          type="text"
+          placeholder="user name"
+          className="border rounded-md p-2 outline-none"
+          {...register("username", {
+            required: "username is required.",
+          })}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          className="border rounded-md p-2 outline-none"
+          {...register("password", {
+            required: "password is required.",
+            min: {value: 6, message: "password should be atleast 6 charachters."}
+          })}
+        />
         <button type="submit" className="p-2 border rounded-md hover:bg-slate-100">
           submit
         </button>
